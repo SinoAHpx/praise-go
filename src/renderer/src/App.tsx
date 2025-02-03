5c887bee959ba9eb8faa672216a6be6411a558b2
AHpxWin <ahpx@yandex.com>
2025-02-03 17:04:59 +0800

    refactor: remove unused useState import in App component

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import usePomodoroStore from './app/pomodoroStore'
 import Pomodoro from './features/Pomodoro'
 import { praise } from './services/llm'
